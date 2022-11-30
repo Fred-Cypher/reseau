4d7a758bf91e9959c0950219743fc768e8a7ac61
Fred Perrier <frederique.perrier@gmail.com>
2022-11-30 16:18:06 +0100

    Modification de la partie blog image qui ne gardait pas la session, OK

--- src/Controller/BlogImagesController.php
+++ src/Controller/BlogImagesController.php
@@ -3,79 +3,76 @@
 namespace App\Controller;
 
 use App\Entity\Images;
-use App\Form\ImagesFormType;
+use App\Form\BlogImagesFormType;
 use App\Repository\ImagesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/images', name: 'images_')]
-class ImagesController extends AbstractController
+#[Route('/blog/images', name: 'images_')]
+class BlogImagesController extends AbstractController
 {
-    #[Route('/', name: 'app_images', methods: ['GET'])]
-    public function newImage(ImagesRepository $imagesRepository): Response
+    #[Route('/', name: 'app_blog_images_index', methods: ['GET'])]
+    public function index(ImagesRepository $imagesRepository): Response
     {
-        return $this->render('sharing/index.html.twig', [
+        return $this->render('blog_images/index.html.twig', [
             'images' => $imagesRepository->findAll(),
         ]);
     }
 
-    #[Route('/new', name: 'app_images_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'app_blog_images_new', methods: ['GET', 'POST'])]
     public function new(Request $request, ImagesRepository $imagesRepository): Response
     {
         $image = new Images();
-        $form = $this->createForm(ImagesFormType::class, $image);
+        $form = $this->createForm(BlogImagesFormType::class, $image);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            //$image->setImageUrl($this->getImageUrl());
             $imagesRepository->save($image, true);
 
-            $this->addFlash('info', 'Votre image a bien été enregistrée, vous pouvez dès à présent la retrouver dans la liste.');
-
-            return $this->redirectToRoute('images_app_images', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_blog_images_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('sharing/new.html.twig', [
+        return $this->renderForm('blog_images/new.html.twig', [
             'image' => $image,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_images_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'app_blog_images_show', methods: ['GET'])]
     public function show(Images $image): Response
     {
-        return $this->render('sharing/show.html.twig', [
+        return $this->render('blog_images/show.html.twig', [
             'image' => $image,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_images_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'app_blog_images_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Images $image, ImagesRepository $imagesRepository): Response
     {
-        $form = $this->createForm(ImagesFormType::class, $image);
+        $form = $this->createForm(BlogImagesFormType::class, $image);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $imagesRepository->save($image, true);
 
-            return $this->redirectToRoute('images_app_images', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_blog_images_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('sharing/edit.html.twig', [
+        return $this->renderForm('blog_images/edit.html.twig', [
             'image' => $image,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_images_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'app_blog_images_delete', methods: ['POST'])]
     public function delete(Request $request, Images $image, ImagesRepository $imagesRepository): Response
     {
         if ($this->isCsrfTokenValid('delete'.$image->getId(), $request->request->get('_token'))) {
             $imagesRepository->remove($image, true);
         }
 
-        return $this->redirectToRoute('images_app_images', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_blog_images_index', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Form/BlogImagesFormType.php
+++ src/Form/BlogImagesFormType.php
@@ -8,7 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ImagesFormType extends AbstractType
+class BlogImagesFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
@@ -25,14 +25,16 @@ class ImagesFormType extends AbstractType
                 ],
                 'label' => 'Chemin de l\'image : '
             ])
-            ->add('description', TextType::class, [
-                'attr' => [
-                    'class' => 'form-control mt-2'
-                ], 
-                'label' => 'Description de l\'image (limité 300 caractères) : '
-            ]
-            )
-        ;
+            ->add(
+                'description',
+                TextType::class,
+                [
+                    'attr' => [
+                        'class' => 'form-control mt-2'
+                    ],
+                    'label' => 'Description de l\'image (limité 300 caractères) : '
+                ]
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
