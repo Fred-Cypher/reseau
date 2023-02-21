667e957adeca9805989e3b8d5195f30d32a08772
Fred Perrier <frederique.perrier@gmail.com>
2023-02-21 14:45:20 +0100

    Division des controllers admin en plusieurs controllers

---
+++ src/Controller/Admin/UsersAdminController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Users;
+use App\Form\Admin\AdminUsersFormType;
+use App\Repository\UsersRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin/users', name: 'admin_users_')]
+class UsersAdminController extends AbstractController
+{
+        // Affichage de le liste des utilisateurs
+    #[Route('/', name: 'index', methods: ['GET'])]
+    public function users(UsersRepository $usersRepository): Response
+    {
+        return $this->render('admin/users/index.html.twig', [
+            'users' => $usersRepository->findAll(),
+        ]);
+    }
+
+    // Affichage de la modification des utilisateurs par un administrateur
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+    public function adminUserEdit(Request $request, Users $users, UsersRepository $usersRepository): Response
+    {   
+        $roles = $this->getParameter('security.role_hierarchy.roles');
+        $form = $this->createForm(AdminUsersFormType::class, $users);
+        $form->handleRequest($request);
+
+        // Vérification que le formulaire est soumis et valide
+        if($form->isSubmitted() && $form->isValid()){
+            $usersRepository->save($users, true);
+
+            $this->addFlash('info', 'L\'utilisateur a bien été modifié');
+
+            return $this->redirectToRoute('admin_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('admin/users/edit.html.twig', [
+            'users' => $users,
+            'roles' => $roles,
+            'form' => $form->createView(),
+        ]);
+    }
+}
